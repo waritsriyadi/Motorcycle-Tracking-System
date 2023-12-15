25f3b4d98d8ddf802e265c9a6b330ca6edcf23c2
waritsriyadi <125733071+waritsriyadi@users.noreply.github.com>
2023-12-16 04:19:09 +0700

    Create post_data_test.php

---
+++ webserver/post_data_test.php
@@ -0,0 +1,22 @@
+<!-- YOU CAN TRY YOUR INPUT HERE -->
+
+php require 'config.php'; ?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Motorcycle Tracking System - Input Mode</h2>
+
+<form method="POST" action="<?php echo POST_DATA_URL;?>">
+  <label for="apikey">Api Key:</label><br>
+  <input type="text" id="api_key" name="api_key" value="<?php echo ESP32_API_KEY;?>"><br>
+  <label for="latitude">Latitude:</label><br>
+  <input type="text" id="lat" name="lat" value="-33.890541"><br>
+  <label for="longitude">Longitude:</label><br>
+  <input type="text" id="lng" name="lng" value="151.274857"><br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+</body>
+</html>
